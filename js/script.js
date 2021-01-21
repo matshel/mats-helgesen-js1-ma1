b9b7387abb32ca125c08b47c48c422e076681ffe
matshel <63284766+matshel@users.noreply.github.com>
2021-01-21 12:09:47 +0100

    delivery

---
+++ js/script.js
@@ -0,0 +1,93 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+// question 1
+
+const cat = {
+    complain: function(){
+        console.log("Meow!");
+    }
+}
+
+cat.complain();
+
+
+// question 2
+
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+// question 3
+
+heading.style.fontSize = "2em";
+
+// question 4
+
+heading.classList.add("content-item");
+
+// question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for(let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+}
+
+// question 6
+
+const resultsContainer = document.querySelector(".results");
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.background = "yellow";
+
+// question 7
+
+function catList(list) {
+
+    for(let i = 0; i < list.length; i++){
+        console.log(list[i].name);
+    }
+}
+
+catList(cats);
+
+// question 8
+
+function createCats(cats) {
+
+    let html = "";
+
+    for(let i = 0; i < cats.length; i++) {
+
+        let catAge = "Age unknown";
+
+        if(cats[i].age) {
+            catAge = cats[i].age;
+        }
+
+        html += `<div>
+                    <h5>${cats[i].name}</h5>
+                    <p>${catAge}</p>
+                </div>`;
+    }
+
+    return html;
+}
+
+const newHTML = createCats(cats);
+
+const catContainer = document.querySelector(".cat-container");
+catContainer.innerHTML = newHTML;
+
+
+// In order to solve these tasks I only used content material from Noroff
